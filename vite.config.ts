cd0e756f64b5a64b102cfc78f17b7ddd256e95b8
Ahmed Zougari <zougarii.ahmed@gmail.com>
2026-04-20 13:18:31 +0100

    fix: hmr for docker compose dev

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     port: 3000,
+    hmr: {
+      host: "localhost",
+    },
   },
   resolve: {
     tsconfigPaths: true,
