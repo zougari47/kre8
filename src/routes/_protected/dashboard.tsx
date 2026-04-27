a083a08085e8ac40bc5c25c710461c0d1d54059c
Ahmed Zougari <zougarii.ahmed@gmail.com>
2026-04-27 11:47:28 +0100

    refactor: change the way we handle loading with authclient

--- src/routes/_protected/dashboard.tsx
+++ src/routes/_protected/dashboard.tsx
@@ -1,6 +1,8 @@
 import { authClient } from "@/lib/auth/client";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
+import { Spinner } from "@/components/ui/spinner";
+
 export const Route = createFileRoute("/_protected/dashboard")({
   component: RouteComponent,
 });
@@ -9,24 +11,19 @@ function RouteComponent() {
   const navigate = useNavigate();
   const { data: session, isPending } = authClient.useSession();
 
+  if (isPending) return <Spinner />;
+
   return (
     <div className="p-8">
       <h1 className="mb-4 text-2xl font-bold">Dashboard</h1>
+
       <div className="space-y-2">
-        {isPending ? (
-          <p>Loading...</p>
-        ) : session ? (
-          <>
-            <p>
-              <strong>Name:</strong> {session.user.name}
-            </p>
-            <p>
-              <strong>Email:</strong> {session.user.email}
-            </p>
-          </>
-        ) : (
-          <p>Not authenticated</p>
-        )}
+        <p>
+          <strong>Name:</strong> {session?.user.name}
+        </p>
+        <p>
+          <strong>Email:</strong> {session?.user.email}
+        </p>
       </div>
       <button
         className="mt-4 rounded bg-red-500 px-4 py-2 text-white"
